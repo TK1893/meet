f1bca90cd64cb71a5b77855f54a1054cd04cd0a4
Tobias Kraft <155923508+TK1893@users.noreply.github.com>
2024-08-28 13:47:32 +0200

    Meet App Ex-4.9

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 // src/api.js
 import mockData from './mock-data';
+import NProgress from 'nprogress';
+
 // import * as atatus from 'atatus-spa';
 
 export const extractLocations = (events) => {
@@ -19,15 +21,26 @@ export const getEvents = async () => {
   if (window.location.href.startsWith('http://localhost')) {
     return mockData;
   }
-
+  if (!navigator.onLine) {
+    const events = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
   const token = await getAccessToken();
 
   if (token) {
     removeQuery();
     const url = 'https://jd7r7l6qek.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/' + token;
+    // const response = await fetch(url);
+    // const result = await response.json();
+    // if (result) {
+    //   return result.events;
+    // } else return null;
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem('lastEvents', JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
