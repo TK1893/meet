98ec4d60c46087b0530751ecdb7cc16d79552f5d
Tobias Kraft <155923508+TK1893@users.noreply.github.com>
2024-08-28 14:15:46 +0200

    Meet App Ex-4.9

--- src/api.js
+++ src/api.js
@@ -2,7 +2,14 @@
 import mockData from './mock-data';
 import NProgress from 'nprogress';
 
-// import * as atatus from 'atatus-spa';
+/**
+ *
+ * @param {*} events:
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ */
 
 export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
@@ -14,6 +21,34 @@ const checkToken = async (accessToken) => {
   const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
   const result = await response.json();
   return result;
+};
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem('access_token');
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get('code');
+    if (!code) {
+      const response = await fetch('https://jd7r7l6qek.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url');
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
+
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const response = await fetch('https://jd7r7l6qek.execute-api.eu-central-1.amazonaws.com/dev/api/token/' + encodeCode);
+  const { access_token } = await response.json();
+  access_token && localStorage.setItem('access_token', access_token);
+
+  return access_token;
 };
 
 // This function will fetch the list of all events
@@ -46,57 +81,6 @@ export const getEvents = async () => {
   }
 };
 
-// // This function will fetch the list of all events
-// export const getEvents = async () => {
-//   try {
-//     if (window.location.href.startsWith('http://localhost')) {
-//       return mockData;
-//     }
-
-//     const token = await getAccessToken();
-
-//     if (token) {
-//       removeQuery();
-//       const url = 'https://jd7r7l6qek.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/' + token;
-//       const response = await fetch(url);
-
-//       if (!response.ok) {
-//         throw new Error(`Fehler beim Abrufen der Events: ${response.statusText}`);
-//       }
-
-//       const result = await response.json();
-//       if (result) {
-//         return result.events;
-//       } else {
-//         throw new Error('Keine Events gefunden.');
-//       }
-//     }
-//   } catch (error) {
-//     console.error('Fehler beim Abrufen der Events:', error);
-//     atatus.notify(error); // Optional: Fehler an Atatus melden
-//     return null;
-//   }
-// };
-
-export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem('access_token');
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get('code');
-    if (!code) {
-      const response = await fetch('https://jd7r7l6qek.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url');
-      const result = await response.json();
-      const { authUrl } = result;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
-};
-
 const removeQuery = () => {
   let newurl;
   if (window.history.pushState && window.location.pathname) {
@@ -108,11 +92,41 @@ const removeQuery = () => {
   }
 };
 
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const response = await fetch('https://jd7r7l6qek.execute-api.eu-central-1.amazonaws.com/dev/api/token/' + encodeCode);
-  const { access_token } = await response.json();
-  access_token && localStorage.setItem('access_token', access_token);
+// export const getAccessToken = async () => {
+//   const accessToken = localStorage.getItem('access_token');
+//   const tokenCheck = accessToken && (await checkToken(accessToken));
 
-  return access_token;
-};
+//   if (!accessToken || tokenCheck.error) {
+//     await localStorage.removeItem('access_token');
+//     const searchParams = new URLSearchParams(window.location.search);
+//     const code = await searchParams.get('code');
+//     if (!code) {
+//       const response = await fetch('https://jd7r7l6qek.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url');
+//       const result = await response.json();
+//       const { authUrl } = result;
+//       return (window.location.href = authUrl);
+//     }
+//     return code && getToken(code);
+//   }
+//   return accessToken;
+// };
+
+// const removeQuery = () => {
+//   let newurl;
+//   if (window.history.pushState && window.location.pathname) {
+//     newurl = window.location.protocol + '//' + window.location.host + window.location.pathname;
+//     window.history.pushState('', '', newurl);
+//   } else {
+//     newurl = window.location.protocol + '//' + window.location.host;
+//     window.history.pushState('', '', newurl);
+//   }
+// };
+
+// const getToken = async (code) => {
+//   const encodeCode = encodeURIComponent(code);
+//   const response = await fetch('https://jd7r7l6qek.execute-api.eu-central-1.amazonaws.com/dev/api/token/' + encodeCode);
+//   const { access_token } = await response.json();
+//   access_token && localStorage.setItem('access_token', access_token);
+
+//   return access_token;
+// };
