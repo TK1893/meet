ec38256ad0486a3d2747ef691067aa5b59ec1a9c
Tobias Kraft <155923508+TK1893@users.noreply.github.com>
2024-09-02 16:12:58 +0200

    Update Meet App Ex.4-10

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -31,7 +31,8 @@ const CityEventsChart = ({ allLocations, events }) => {
       >
         <CartesianGrid />
         <XAxis type="category" dataKey="city" name="City" />
-        <YAxis type="number" dataKey="countnumber" name="Number of events" allowDecimals={false} />        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
       </ScatterChart>
     </ResponsiveContainer>
