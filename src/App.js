41acfa326b2400fc5147a233b94ed9cda1a01e8d
Tobias Kraft <155923508+TK1893@users.noreply.github.com>
2024-09-04 00:10:38 +0200

    Update Meet App Ex.4-10

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
-import './App.css';
+import './App.scss';
 
 const App = () => {
   const [allLocations, setAllLocations] = useState([]);
@@ -39,7 +39,10 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>MEET EVENTS</h1>
+      <div className="title-wrapper">
+        <h1>MEET</h1>
+        <h2>Coding Events</h2>
+      </div>
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlertText.length ? <ErrorAlert text={errorAlertText} /> : null}
