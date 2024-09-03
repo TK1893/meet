d24a6fbde08a7f4e98318d2a34360d5eb05d40ab
Tobias Kraft <155923508+TK1893@users.noreply.github.com>
2024-09-03 12:43:05 +0200

    Update Meet App Ex.4-10

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -6,7 +6,7 @@ import { ResponsiveContainer, PieChart, Pie, Legend, Cell } from 'recharts';
 
 function EventGenresChart({ events }) {
   const [data, setData] = useState([]);
-  const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
+  const genres = ['React', 'Node', 'jQuery', 'Angular', 'JavaScript'];
   const colors = ['#ffb3ba', '#bae1ff', '#b5f8c3', '#ffc583', '#B687FF'];
 
   useEffect(() => {
