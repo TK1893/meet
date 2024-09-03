dc3f95c6bdd807a7f590b7673de218700826d24d
Tobias Kraft <155923508+TK1893@users.noreply.github.com>
2024-09-03 11:25:07 +0200

    Update Meet App Ex.4-10

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -26,7 +26,7 @@ const CityEventsChart = ({ allLocations, events }) => {
           top: 20,
           right: 20,
           bottom: 60,
-          left: -20,
+          left: -30,
         }}
       >
         <CartesianGrid />
